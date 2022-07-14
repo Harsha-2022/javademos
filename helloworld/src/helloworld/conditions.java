2e11ca66f7637364f2eb31b53a8f532a9a96d588
Harsha <yhc9520@gmail.com>
2022-07-14 10:38:13 +0530

    changes

---
+++ helloworld/src/helloworld/conditions.java
@@ -0,0 +1,22 @@
+package helloworld;
+
+public class conditions {
+
+	public static void main(String[] args) {
+         boolean isalien=false;
+         if (isalien){
+        	 System.out.println("it is an alien");
+         }
+         if(!isalien) {
+        	 System.out.println("it is not an alien");
+         }
+         boolean mybool= false;
+         if(mybool=true) {
+        	 System.out.println("x");
+         }
+         else {
+        	 System.out.println("y");
+         }
+	}
+
+}
