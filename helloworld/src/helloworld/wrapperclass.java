bd43bc58d5a639a1445bc4f30df0af60089ca8a2
Harsha <yhc9520@gmail.com>
2022-07-14 10:39:18 +0530

    changes

---
+++ helloworld/src/helloworld/wrapperclass.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class wrapperclass {
+     public static void main(String[] args) {
+    	 int y=5;
+    	 Integer x=2;
+    	 Integer z=new Integer(5);
+    	 int myint=z;
+    	 short ynewshort=3276;
+    	 short newshort=ynewshort;
+     }
+}
